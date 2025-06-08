2836f635c11e59aec9e7a37cc8d0ea59596d727d
Rumbile <gmusuvadi@gmail.com>
2025-06-08 21:04:22 +0400

    Add files via upload

---
+++ stringuppercase.py
@@ -0,0 +1,10 @@
+class IOString():
+    def _init_(self):
+        self.string1 = ""
+    def get_String(self):
+        self.string1 = input("enter string: ")
+    def print_String(self):
+        print("result is:" self.strl.upper())
+str1 = IOString()
+str1.get_String()
+str1.print_String()
